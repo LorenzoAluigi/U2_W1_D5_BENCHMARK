940d6f5684308318f76a788070ec3bfded3f160d
LorenzoAluigi <lorenzoaluigi@icloud.com>
2023-06-25 00:06:55 +0200

    css responsive start

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 console.log("hello word!");
 
 const navbar = document.getElementById("navbar");
+const navstart = document.getElementById ('started')
 
 document.addEventListener("scroll", function () {
   console.log(window.scrollY);
   if (window.scrollY > 300) {
     navbar.classList.add("scroll-navbar");
+    navstart.classList.add ('start-scroll')
   } else {
     navbar.classList.remove("scroll-navbar");
+    navstart.classList.remove ('start-scroll')
   }
 });
