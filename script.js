d0b4c5cbc99acfc5cf5ae5e562e5d8789aeb8d84
LorenzoAluigi <134380077+LorenzoAluigi@users.noreply.github.com>
2023-06-25 15:06:07 +0200

    extra 1 completed

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 console.log("hello word!");
 
 const navbar = document.getElementById("navbar");
-const navstart = document.getElementById ('started')
+const navstart = document.getElementById("started");
 
+console.log(innerWidth);
 document.addEventListener("scroll", function () {
   console.log(window.scrollY);
-  if (window.scrollY > 300) {
+  if (innerWidth < 476 && window.scrollY > 220) {
     navbar.classList.add("scroll-navbar");
-    navstart.classList.add ('start-scroll')
+    navstart.classList.add("start-scroll");
+  } else if (window.scrollY > 300) {
+    navbar.classList.add("scroll-navbar");
+    navstart.classList.add("start-scroll");
   } else {
     navbar.classList.remove("scroll-navbar");
-    navstart.classList.remove ('start-scroll')
+    navstart.classList.remove("start-scroll");
   }
 });
